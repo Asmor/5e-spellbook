8f20ff5a1ce33e86f3c850af3b635078edbbc480
asmor <itoltz@gmail.com>
2015-04-03 20:18:00 -0400

    Blinds working

--- components/spells/spells.js
+++ components/spells/spells.js
@@ -3,6 +3,7 @@
 angular.module("spells", [])
 .service("classService", ["$http", function ($http) {
 	var classService = {
+		classes: {},
 		classesBySpell: {},
 	};
 
@@ -18,6 +19,7 @@ angular.module("spells", [])
 	].forEach(function (className) {
 		$http.get("data/" + className + ".json").then(function (response) {
 			registerClass(response.data);
+			classService.classes[response.data.name] = response.data;
 		});
 	});
 
@@ -49,6 +51,23 @@ angular.module("spells", [])
 .service("spellService", ["$http", function ($http) {
 	var spellService = {
 		all: [],
+		filters: {
+			className: "",
+			level: {
+				min: 0,
+				max: 9,
+			},
+			schools: {
+				"Abjuration": true,
+				"Conjuration": true,
+				"Divination": true,
+				"Enchantment": true,
+				"Evovation": true,
+				"Illusion": true,
+				"Necromancy": true,
+				"Transmutation": true,
+			},
+		},
 	};
 
 	$http.get("data/spells.json").then(function (response) {
@@ -63,7 +82,7 @@ angular.module("spells", [])
 		templateUrl: "components/spells/spellList.html",
 		link: function (scope) {
 			scope.spells = spellService;
-			scope.sortType = "school";
+			scope.sortType = "level";
 			scope.sortReverse = false;
 			scope.classService = classService;
 			scope.setSortType = function setSortType(type) {
@@ -76,6 +95,75 @@ angular.module("spells", [])
 				scope.sortType = type;
 			};
 		}
+	};
+}])
+.directive("filters", ["classService", "spellService", function (classService, spellService) {
+	return {
+		scope: {},
+		templateUrl: "components/spells/filters.html",
+		link: function (scope) {
+			scope.classService = classService;
+			scope.spells = spellService;
+			scope.showFilter = "class";
+
+			scope.getMetaFiltersTitle = getMetaFiltersTitle;
+			scope.setSchoolExclusive = setSchoolExclusive;
+			scope.setAllSchools = setAllSchools;
+		},
+	};
+
+	function setAllSchools(val) {
+		Object.keys(spellService.filters.schools).forEach(function (key) {
+			spellService.filters.schools[key] = val;
+		});
+	}
+
+	function setSchoolExclusive(school) {
+		setAllSchools(false);
+		spellService.filters.schools[school] = true;
+	}
+
+	function getMetaFiltersTitle() {
+		var f = spellService.filters;
+		var enabledFilters = [];
+		var chosenSchools = _.invert(f.schools, true)['true'] || [];
+
+		if ( (f.level.min > 0) || (f.level.max < 9) ) {
+			if ( f.level.min === f.level.max ) {
+				enabledFilters.push("Lvl " + f.level.min);
+			} else if ( f.level.min === 0 ) {
+				enabledFilters.push("Lvl <= " + f.level.max);
+			} else if ( f.level.max === 9 ) {
+				enabledFilters.push("Lvl >= " + f.level.min);
+			} else {
+				enabledFilters.push(["Lvl", f.level.min, "-", f.level.max].join(" "));
+			}
+		}
+
+		if ( chosenSchools.length && (chosenSchools.length < Object.keys(f.schools).length) ) {
+			if ( chosenSchools.length === 1 ) {
+				enabledFilters.push(chosenSchools[0]);
+			} else {
+				enabledFilters.push(_.map(chosenSchools, function(s) { return s.substring(0, 2); }).join(", "));
+			}
+		}
+
+		if ( !enabledFilters.length ) {
+			enabledFilters.push("Spell filters");
+		}
+
+		return enabledFilters.join(" ");
+	}
+}])
+.directive("filterBlock", [function () {
+	return {
+		scope: {
+			title: "=",
+			name: "@",
+			filterProp: "=",
+		},
+		templateUrl:  "components/spells/filter-block.html",
+		transclude: true,
 	};
 }])
 .filter("conjugateSpellLevel", [function () {
@@ -143,6 +231,43 @@ angular.module("spells", [])
 		}
 
 		return 0;
+	}
+}])
+.filter("spellFilter", ["classService", "spellService", function (classService, spellService) {
+window.spells = spellService;
+window.classes = classService;
+	return function (spells) {
+		var out = [], i;
+
+		for ( i = 0; i < spells.length; i++ ) {
+			if ( checkFilters(spells[i]) ) {
+				out.push(spells[i]);
+			}
+
+		}
+
+		return out;
+	};
+
+	function checkFilters(spell) {
+		var checkClasses = spellService.filters.className.split(":"),
+			i;
+
+		if ( checkClasses[1] ) {
+			checkClasses[1] = checkClasses[0] + " (" + checkClasses[1] + ")";
+		}
+
+		if ( spellService.filters.className ) {
+			for ( i = 0; i < checkClasses.length; i++ ) {
+				if ( classService.classesBySpell[spell.name].indexOf(checkClasses[i]) > -1 ) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		return true;
 	}
 }])
 ;
